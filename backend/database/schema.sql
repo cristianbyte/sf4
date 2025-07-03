025570a342dd9fa0b43076f4c51ff9e0509f9143
cristianbyte <charlsmaritz@gmail.com>
2025-07-03 09:12:23 -0500

    progress

---
+++ backend/database/schema.sql
@@ -0,0 +1,12 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CREATE INDEX idx_users_email ON users(email);
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,14 @@
 import express from 'express'
-import { PORT } from './config'
+import { PORT } from './config/config.js'
 
 const app = express()
 
 app.disable('x-powered-by')
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!')
-})
+app.post('/login', (req, res) => {})
+app.post('/signin', (req, res) => {})
+app.post('/logout', (req, res) => {})
 
 app.listen(PORT, () => {
-  console.log(`Server is running on: http://localhost:${PORT}`);
+  console.log(`Server is running on: http://localhost:${PORT}`)
 })
