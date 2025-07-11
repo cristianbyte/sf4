c6cfa2809cf4b9bb5bc634afa8229bac7151de85
cristianbyte <charlsmaritz@gmail.com>
2025-07-11 15:21:40 -0500

    Refactor user tests to include user creation and deletion scenarios, and fix test for existing email conflict

--- backend/test/user.test.js
+++ backend/test/user.test.js
@@ -3,18 +3,36 @@ import assert from 'node:assert';
 import request from 'supertest';
 import app from '../index.js';
 
-describe('POST /api/user/login', () => {
-  test('should return 200 and a token if credentials are valid', async () => {
+let userId;
+let cookie;
+let userTest = {
+  name: 'Camile',
+  email: 'camile@example.com',
+  password: 'passStrong67',
+}
+describe('User login and deletion tests', () => {
+  test('should create a user (201)', async () => {
     const response = await request(app)
-      .post('/api/user/login')
-      .send({
-        email: 'john.doe@example.com',
-        password: 'Pass123s'
-      });
+      .post('/api/user')
+      .send(userTest);
 
-    assert.strictEqual(response.statusCode, 200);
-    assert.ok(response.body.hasOwnProperty('id'));
+    assert.strictEqual(response.statusCode, 201);
+    assert.deepStrictEqual(
+      Object.keys(response.body).sort(),
+      ['id', 'name', 'email', 'created_at'].sort()
+    );
+
+    userId = response.body.id;
   });
+
+  test('shuld return 409: Conflict: Email already exists', async () => {
+    const response = await request(app)
+      .post('/api/user')
+      .send(userTest)
+
+    assert.strictEqual(response.statusCode, 409);
+    assert.ok(response.body.hasOwnProperty('error'));
+  })
 
   test('should return 400 if credentials are invalid', async () => {
     const response = await request(app)
@@ -26,5 +44,28 @@ describe('POST /api/user/login', () => {
 
     assert.strictEqual(response.statusCode, 400);
     assert.ok(response.body.hasOwnProperty('error'));
+  });
+
+  test('should return 200 and a token if credentials are valid', async () => {
+    const response = await request(app)
+      .post('/api/user/login')
+      .send({
+        email: 'camile@example.com',
+        password: 'passStrong67'
+      });
+
+    assert.strictEqual(response.statusCode, 200);
+
+    cookie = response.headers['set-cookie']?.[0];
+    assert.ok(cookie, 'Cookie should be returned');
+  });
+
+
+  test('should delete the user (204)', async () => {
+    const response = await request(app)
+      .delete(`/api/user/${userId}`)
+      .set('Cookie', cookie);
+
+    assert.strictEqual(response.status, 204);
   });
 });
