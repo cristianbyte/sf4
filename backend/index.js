5661b471b70e9a2a3f9f174fc0c204436d087aab
cristianbyte <charlsmaritz@gmail.com>
2025-08-24 02:02:30 -0500

    Add Socket.IO for real-time communication;

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,17 @@ import vote from './src/routes/vote.js'
 import { jsonSyntaxErrorHandler, errorHandler } from './src/middlewares/errorHandler.js'
 import cookieParser from 'cookie-parser'
 import { methodNotAllowedHandler, notFoundHandler } from './src/middlewares/badRequest.js'
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', () =>{
+  console.log('User connected');
+})
+
 app.disable('x-powered-by')
 
 // Routes
@@ -28,7 +37,7 @@ app.use(jsonSyntaxErrorHandler)
 app.use(errorHandler)
 
 await initDatabase();
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server is running on: http://localhost:${PORT}`)
 })
 
