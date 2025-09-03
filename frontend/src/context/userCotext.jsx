ac8f5c00b79277bd21e64956bdfc73d87cbef93a
cristianbyte <charlsmaritz@gmail.com>
2025-09-03 01:14:37 -0500

    Enhance error handling response format; add status code to JSON response, and remove anonymous user log message. Update Button component's rel attribute for security. Implement guest user creation with random names in UserContext. Refactor Menu layout and CSS for improved button styling.

--- frontend/src/context/userCotext.jsx
+++ frontend/src/context/userCotext.jsx
@@ -1,33 +1,52 @@
 import { createContext, useContext, useState, useEffect } from "react";
-import { apiRequest } from "../services/request";
+import { apiRequest, clearCache } from "../services/request";
 const UserContext = createContext();
 const USER_STORAGE_KEY = 'sf4_user_data';
 
 export function UserProvider({ children }) {
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const randomNames = ["Invitado", "Visitante", "Participante", "Luchador", "Retador", "Espectador", "Explorador", "Aliado", "Observador", "Curioso", "Anonimo"];
 
-useEffect(() => {
-  const fetchUser = async () => {
-    setIsLoading(true);
-    try {
-      const getUser = await apiRequest('/user/me', "GET");
-      if (getUser) {
-        setUser(getUser);
-      }
-    } catch (error) {
-      if (error.status === 401) {
-        setUser(null);
-      } else {
-        console.error("Unexpected error fetching user:", error);
-      }
-    } finally {
-      setIsLoading(false);
-    }
-  };
+  useEffect(() => {
+    const fetchUser = async () => {
+      setIsLoading(true);
+      try {
+        clearCache('/user/me');
+        const getUser = await apiRequest('/user/me', "GET");
+        if (getUser) {
+          setUser(getUser);
+        }
+        console.log("Guest user created:", getUser);
 
-  fetchUser();
-}, []);
+      } catch (error) {
+        if (error.response.data.status === 401) {
+          try {
+            const randomName = `${randomNames[Math.floor(Math.random() * randomNames.length)]}${Math.floor(Math.random() * 1000)}`;
+
+            const newGuest = await apiRequest('/user/guest', "POST", {
+              name: randomName,
+              role: "GUEST"
+            });
+
+            if (newGuest) {
+              setUser(newGuest);
+            }
+            console.log("Guest user created:", newGuest);
+          } catch (guestError) {
+            console.error("Error creating guest:", guestError);
+          }
+        } else {
+          console.error("Unexpected error fetching user:", error);
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchUser();
+  }, []);
+
 
   const updateUser = (userData) => {
     setUser(userData);
