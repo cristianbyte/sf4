989487226d32ca0e64381e570945d6dcebd7928e
cristianbyte <charlsmaritz@gmail.com>
2025-09-04 11:52:34 -0500

    details

--- frontend/src/layouts/Menu.jsx
+++ frontend/src/layouts/Menu.jsx
@@ -1,7 +1,9 @@
-import { useState } from "react";
+import { useLocation } from "react-router-dom";
+import { useState, useEffect } from "react";
 import { useUser } from '../context/userCotext';
 import { useViewTransitionNavigate } from "../hooks/useViewTransitionNav";
 import Button from "../components/common/Button";
+import SF4 from "../assets/images/SF4-logo.webp";
 import AuthModal from "../components/auth/AuthModal";
 import './menu.css';
 
@@ -9,10 +11,27 @@ const Menu = () => {
     const [showAuth, setShowAuth] = useState(false);
     const { user, logout } = useUser();
     const transitionNavigate = useViewTransitionNavigate();
+    const [scrolled, setScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrolled(window.scrollY > 30);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
+    function useOnPrincipalPage() {
+        const location = useLocation();
+        return location.pathname === "/";
+    }
+
+    const onPrincipalPage = useOnPrincipalPage();
 
 
     return (
-        <div className="menu__container">
+        <div className={scrolled ? "scrolled menu__container" : "menu__container"}>
             {showAuth && (
                 <AuthModal
                     isOpen={showAuth}
@@ -33,6 +52,12 @@ const Menu = () => {
                     onClick={(e) => { transitionNavigate(e, '/') }}
                     children={"COMBATES"}
                 />
+                {!onPrincipalPage && (<div
+                    onClick={(e) => { transitionNavigate(e, '/') }}
+                    className="menu__logo-container"
+                >
+                    <img src={SF4} alt="Stream Fighters 4" className="menu__logo" />
+                </div>)}
                 <Button
                     size="sizeS"
                     href={"https://www.taquillalive.com/book-performance/?artist=stream-fighters&event=TCL.EVN885.PRF1"}
