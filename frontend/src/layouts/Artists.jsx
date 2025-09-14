3b6f3e8059b8f955bcfbdade0d85cd66e8f3b628
cristianbyte <charlsmaritz@gmail.com>
2025-09-14 01:03:50 -0500

    feat: replace F-pose image with RC-pose and enhance artist layout styles

--- frontend/src/layouts/Artists.jsx
+++ frontend/src/layouts/Artists.jsx
@@ -1,21 +1,24 @@
 import './artists.css'
 import Cosculluela from '../assets/images/Cosculluela-pose.png'
 import NJ from '../assets/images/NJ-pose.png'
-import F from '../assets/images/F-pose.png'
+import RC from '../assets/images/RC-pose.png'
 
 const Artist = () => {
     return (
         <div className="artist-container">
+            <h2 className="artists-title shine-low">
+                Artistas Invitados
+            </h2>
             <div className="artist" id="artists">
                 <div className="artist-left">
                     <img src={NJ} alt="artista" />
                 </div>
                 <div className="artists-principal">
                     <img src={Cosculluela} alt="Cosculluela" />
-                    <div className="artist-name"></div>
+                    <div className="artist-name">Cosculluela</div>
                 </div>
                 <div className="artist-right">
-                    <img src={F} alt="artista" />
+                    <img src={RC} alt="artista" />
                 </div>
             </div>
         </div>
